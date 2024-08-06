f2333654e548402e652d7eddbaa62a3c5fcd6afc
haseebjanhamraz <haseebjanhamraz@gmail.com>
2024-08-06 14:25:42 +0500

    "Added Error404 component to display when vaccineRecord is empty, and wrapped table with conditional statement to render only when vaccineRecord has data."

--- frontend/src/components/VaccinationRecords.jsx
+++ frontend/src/components/VaccinationRecords.jsx
@@ -1,6 +1,7 @@
 // src/components/VaccinationRecords.jsx
 import React from "react";
 import { formatDate } from "../utils/formatDate";
+import Error404 from "../components/Error404";
 
 const VaccinationRecords = ({ vaccineRecord }) => {
   if (!vaccineRecord) {
@@ -13,28 +14,32 @@ const VaccinationRecords = ({ vaccineRecord }) => {
         Vaccination Records
       </h2>
       <div className="overflow-x-auto">
-        <table className="min-w-full border-collapse border border-gray-300 overflow-x-auto">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 p-2 hidden sm:table-cell">
-                Vaccine
-              </th>
-              <th className="border border-gray-300 p-2">Injected On</th>
-            </tr>
-          </thead>
-          <tbody className="text-center">
-            {vaccineRecord.map((vaccine, index) => (
-              <tr key={vaccine._id} className="border border-gray-300">
-                <td className="border border-gray-300 p-2 hidden sm:table-cell">
-                  {vaccine.vaccineName}
-                </td>
-                <td className="border border-gray-300 p-2">
-                  {formatDate(vaccine.date)}
-                </td>
+        {vaccineRecord.length > 0 ? (
+          <table className="min-w-full border-collapse border border-gray-300 overflow-x-auto">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 p-2 hidden sm:table-cell">
+                  Vaccine
+                </th>
+                <th className="border border-gray-300 p-2">Injected On</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="text-center">
+              {vaccineRecord.map((vaccine, index) => (
+                <tr key={vaccine._id} className="border border-gray-300">
+                  <td className="border border-gray-300 p-2 hidden sm:table-cell">
+                    {vaccine.vaccineName}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {formatDate(vaccine.date)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <Error404 />
+        )}
       </div>
     </div>
   );
